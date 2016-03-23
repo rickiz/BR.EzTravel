4a4e27b8d75c79c11a1bf46efa79427eadc07f22
rickiz85@gmail.com <rickiz85@gmail.com>
2016-03-24 03:45:08 +0800

    Fix

---
+++ BR.EzTravel.Web/Models/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BR.EzTravel.Web.Models
+{
+    public enum Language
+    {
+        EN, CN
+    }
+
+    public enum ListType
+    {
+        Country, Category, PackageCategory
+    }
+}
