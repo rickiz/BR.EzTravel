d69dcbf031335076aaa3599a13dcdc836761b307
Rick Lee <rickiz85@gmail.com>
2016-04-06 11:41:26 +0800

    added adminMember controller

---
+++ BR.EzTravel.Web/Areas/EN/Controllers/AdminMemberController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BR.EzTravel.Web.Areas.EN.Controllers
+{
+    public class AdminMemberController : Controller
+    {
+        // GET: EN/AdminMember
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
