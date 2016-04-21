daeb49f79bf20f4612cccf3594eb61502ff2240a
bzai123 <bzai123@gmail.com>
2016-04-21 12:36:07 +0800

    checked in contact us

---
+++ BR.EzTravel.Web/Models/ContactUsViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BR.EzTravel.Web.Models
+{
+    public class ContactUsViewModels
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string Comments { get; set; }
+    }
+}
